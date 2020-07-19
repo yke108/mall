54aa70ec5084ec0a75fb3cb688ce3f7e4ce2d267
yke108 <623756770@qq.com>
2020-07-19 18:36:48 +0800

    m

--- application/database.php
+++ application/database.php
@@ -17,7 +17,7 @@ return [
     // 数据库名
     'database'        => 'mall',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'mall',
     // 密码
     'password'        => '123456',
     // 端口
